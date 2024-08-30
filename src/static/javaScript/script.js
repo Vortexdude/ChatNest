75963990dc34faccf984843771a25d7409f5248d
vortexdude <nitinnamdeo456@gmail.com>
2024-08-30 23:44:31 +0530

    :loudspeaker: sends json data into the websocket

--- src/static/javaScript/script.js
+++ src/static/javaScript/script.js
@@ -1,22 +1,30 @@
 var client_id = Date.now()
 const websocket_host = "192.168.1.76"
 const websocket_port = 5000
-var websocket_endpoint = `ws://${websocket_host}:${websocket_port}/ws/${client_id}`
+var websocket_endpoint = `ws://${websocket_host}:${websocket_port}/ws`
 
 document.querySelector("#ws-id").textContent = client_id;
 var ws = new WebSocket(websocket_endpoint);
+
+
+
 ws.onmessage = function(event) {
     var messages = document.getElementById('messages')
     var message = document.createElement('li')
     var message_span = document.createElement('span')
-    var content = document.createTextNode(event.data)
+    var content = document.createTextNode(event.data.message)
     message_span.appendChild(content)
     message.appendChild(message_span)
     messages.appendChild(message)
 };
 function sendMessage(event) {
     var input = document.getElementById("messageText")
-    ws.send(input.value)
+    var data = JSON.stringify({
+        client_id: client_id,
+        email: "no-reply@chatnest.com",
+        message: input.value
+    });
+    ws.send(data)
     input.value = ''
     event.preventDefault()
 }
